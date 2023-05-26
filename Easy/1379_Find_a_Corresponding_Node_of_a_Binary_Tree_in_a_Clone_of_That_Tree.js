5fdc2f8a88b475a14659654ce20780534a6f5ea2
frntprog <frntprog@gmail.com>
2023-05-26 21:51:03 +0300

    doen problem

---
+++ Easy/1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function (original, cloned, target) {};
