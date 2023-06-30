7f7b0773b62bdf769aec2efc8146fef40104506c
frntprog <frntprog@gmail.com>
2023-06-30 10:37:44 +0300

    done

---
+++ Easy/203_Remove_Linked_List_Elements.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  var fake = new ListNode(0);
+  fake.next = head;
+  var curr = fake;
+  while (curr.next != null) {
+    if (curr.next.val == val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return fake.next;
+};
